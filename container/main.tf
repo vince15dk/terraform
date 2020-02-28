7c777541585cd2ab18d33b5ff04121d54a09a8df
Your Name <you@example.com>
2020-02-28 14:42:15 +0900

    first commit

---
+++ container/main.tf
@@ -0,0 +1,10 @@
+resource "docker_container" "container_id" {
+  name = "${var.name}"
+  image = "${var.image}"
+  ports {
+    internal = "${var.int_port}"
+    external = "${var.ext_port}"
+  }
+}
+
+
